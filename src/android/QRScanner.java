b24be5c22a963570553c618dfbf0e01f1059024b
Cesar Augusto <cesar.aug.eng@gmail.com>
2019-04-02 18:48:33 -0400

    Mejoras

--- src/android/QRScanner.java
+++ src/android/QRScanner.java
@@ -5,6 +5,8 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.apache.cordova.PluginResult.Status;
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class QRScanner extends CordovaPlugin {
 
