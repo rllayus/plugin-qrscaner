fa39abce14db99dcc0578ee45a6badb6af4b2d4b
Cesar Augusto <cesar.aug.eng@gmail.com>
2019-04-03 02:42:50 -0400

    Mejoras

--- src/android/QRScanner.java
+++ src/android/QRScanner.java
@@ -18,7 +18,7 @@ public class QRScanner extends CordovaPlugin {
             if ("doConnect".equals(action)) {
                 //accion ejecutada
                 PluginResult pluginResult = new PluginResult(
-                        PluginResult.Status.OK, "Plugin executed");
+                        PluginResult.Status.OK, "B/FuHXl3ha99gGFM/XSyCTSzrWBGMVMofBx6FcvGcQhTZ4lAEvppCSwP7GVmv6kRiFa89REggMwsXbNoc7cZakIgODvEPpFCGrUreNcsxxvuq8vbO/B2rah45Gl2phvQIMDjJ114+LSxYIWw+QK4CLHOqLlvvVErg3MYCrfaUk2dqZ9ZXGZuwFJ5GZf5PIjH1QrnxQiFSzEeqDLDYanljhPJis1t5PFI8Z1Z8yGtjEL+xb6GHVyecv4dtiAeFsRKs2g2EZ/4zrRSS7i5WQoEafhW4gzAfYYpKlXkUX6jdyoepJMEefsjNN27YrGufq9anKpkeWK274sRfigoFINGvw==|cc44ff5fd40ea772");
                 pluginResult.setKeepCallback(true);
                 callbackContext.sendPluginResult(pluginResult);
                 success = true;
