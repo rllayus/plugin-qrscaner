509125909cd0ebbe5337a16e0099746bf10cd4f7
Cesar Augusto <cesar.aug.eng@gmail.com>
2019-04-05 02:35:34 -0400

    Agregando funciones

--- www/QRScanner.js
+++ www/QRScanner.js
@@ -1,27 +1,94 @@
 var QRScanner = {
-    scan: function(userid, otherparam, successCallback, errorCallback) {
+    scan: function(successCallback, errorCallback) {
         cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
             'QRScanner', // mapped to our native Java class
             'scan', // with this action name
-            [{                  // and this array of custom arguments to create our entry
-                "userid": userid,
-                "otherparam": otherparam
-            }]
+            []
         );
     },
-    show: function(userid, otherparam, successCallback, errorCallback) {
+    show: function(successCallback, errorCallback) {
         cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
             'QRScanner', // mapped to our native Java class
             'show', // with this action name
-            [{                  // and this array of custom arguments to create our entry
-                "userid": userid,
-                "otherparam": otherparam
-            }]
+            []
+        );
+    },
+    cancelScan: function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'QRScanner', // mapped to our native Java class
+            'cancelScan', // with this action name
+            []
+        );
+    },
+    openSettings: function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'QRScanner', // mapped to our native Java class
+            'openSettings', // with this action name
+            []
+        );
+    },
+    pausePreview: function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'QRScanner', // mapped to our native Java class
+            'pausePreview', // with this action name
+            []
+        );
+    },
+    enableLight: function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'QRScanner', // mapped to our native Java class
+            'enableLight', // with this action name
+            []
+        );
+    },
+    disableLight: function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'QRScanner', // mapped to our native Java class
+            'disableLight', // with this action name
+            []
+        );
+    },
+    prepare: function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'QRScanner', // mapped to our native Java class
+            'prepare', // with this action name
+            []
+        );
+    },
+    destroy: function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'QRScanner', // mapped to our native Java class
+            'destroy', // with this action name
+            []
+        );
+    },
+    getStatus: function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'QRScanner', // mapped to our native Java class
+            'getStatus', // with this action name
+            []
         );
     }
+
 }
 module.exports = QRScanner
